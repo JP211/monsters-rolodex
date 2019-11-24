c955a188221594f2d85acfa3f1a426db25706b1e
Jessica Cockerell <jessica@MacBook-Air.local>
2019-11-24 11:58:59 -0600

    Updates

---
+++ precache-manifest.8202a0960656afbea082b33a455eaab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba402aa7b9747dafa8089c1284dd8fa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84b830a3e477abc795e8",
+    "url": "/monsters-rolodex/static/css/main.ab1607db.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "84b830a3e477abc795e8",
+    "url": "/monsters-rolodex/static/js/main.bf952ed8.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
